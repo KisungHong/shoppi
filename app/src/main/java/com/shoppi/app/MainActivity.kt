98f2187ee8fe583321b9c5548ef39d2ca8548f68
kisung <ppter0311@naver.com>
2022-12-13 15:41:39 +0900

    [섹션5] Fragment 추가 및 화면 연결

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.shoppi.app
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        setContentView(R.layout.activity_main)
+
     }
 }
